c707f1e1df2f09477f6bb6d7dce565cc459fbdda
Grant-Ellington <gellingtonem6@gmail.com>
2023-02-12 20:45:47 -0600

    added code to solve chessbaord

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('hello world');
+
+
+//console.log('hello poop');
+//for( let line = ''; line.length< 8; line+= '#')
+//console.log(line)
+
+//fiz buzz problem
+
+// for(let number = 0; number < 101; number+=1)
+// if(number%3 === 0 && number%5 === 0){
+//     console.log('fizzBuzz')
+// }else if( number%3 === 0){
+//     console.log('buzz')
+// }else if(number%5===0){
+//     console.log('fizz')
+// }else{console.log(number)}
+
+let chessboard = (row, column) => {
+  
+    for(let i = 0; i < row; i++){
+      //If odd then start with ' '
+      //Else start with '#'
+      let start = i % 2 === 1 ? ' ' : '#';
+      
+      //Temp str to stor the pattern of the column
+      let str = '';
+      for(let j = 0; j < column; j++){
+        //Swap current pattern with another one in each iteration
+        start = start == '#' ? ' ' : '#';
+        str += start;
+      }
+      
+      //Print the pattern for the current row
+      console.log(str);
+    }
+  }
+
+  chessboard(10,10)
